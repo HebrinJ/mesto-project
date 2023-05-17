0a0cdbe36b731ed997169fdeb4ae7e5a00df3f8f
Glazkov Sergey <glaz.dev@yandex.ru>
2023-05-17 12:15:54 +0300

    feat: Popup class

--- src/components/api.js
+++ src/components/api.js
@@ -3,89 +3,85 @@ import { checkResponse } from "./utils";
 const config = {
     baseUrl: `https://nomoreparties.co/v1/plus-cohort-23`,
     headers: {
-        authorization: '020effc4-1211-4deb-93d9-11a33dcdf1a5',
-        'Content-Type': 'application/json',
+        authorization: "020effc4-1211-4deb-93d9-11a33dcdf1a5",
+        "Content-Type": "application/json",
     },
 };
 
 class Api {
-    constructor({baseUrl, headers}) {
+    constructor({ baseUrl, headers }) {
         this._baseUrl = baseUrl;
         this._headers = headers;
     }
 
     _request(linkEndpoint, options) {
-        return fetch(this._baseUrl+linkEndpoint, options).then(checkResponse);
+        return fetch(this._baseUrl + linkEndpoint, options).then(checkResponse);
     }
 
     getProfileData() {
-        return this._request(`/users/me`, { 
-            headers: this._headers
-        })    
+        return this._request(`/users/me`, {
+            headers: this._headers,
+        });
     }
-    
+
     getCards() {
         return this._request(`/cards`, {
-            headers: this._headers
-        })
+            headers: this._headers,
+        });
     }
-    
+
     editProfileData(newName, newMajor) {
         return this._request(`/users/me`, {
-            method: 'PATCH',
+            method: "PATCH",
             headers: this._headers,
             body: JSON.stringify({
                 name: newName,
-                about: newMajor
-            })
+                about: newMajor,
+            }),
         });
     }
-    
+
     sendNewCard(cardLink, cardName) {
         return this._request(`/cards`, {
-            method: 'POST',
+            method: "POST",
             headers: this._headers,
             body: JSON.stringify({
-              name: cardName,
-              link: cardLink
-            })
-        })
+                name: cardName,
+                link: cardLink,
+            }),
+        });
     }
-    
+
     deleteCard(cardId) {
         return this._request(`/cards/${cardId}`, {
-                method: 'DELETE',
-                headers: this._headers
-        })
+            method: "DELETE",
+            headers: this._headers,
+        });
     }
-    
+
     setLike(cardId) {
-        return this._request(`/cards/likes/${cardId}`, { 
-            method: 'PUT',
-            headers: this._headers
-        })  
+        return this._request(`/cards/likes/${cardId}`, {
+            method: "PUT",
+            headers: this._headers,
+        });
     }
-    
+
     removeLike(cardId) {
-        return this._request(`/cards/likes/${cardId}`, { 
-            method: 'DELETE',
-            headers: this._headers
-        })  
+        return this._request(`/cards/likes/${cardId}`, {
+            method: "DELETE",
+            headers: this._headers,
+        });
     }
-    
-    changeAvatar(avatarLink) {    
-        return this._request(`/users/me/avatar`, { 
-            method: 'PATCH',
+
+    changeAvatar(avatarLink) {
+        return this._request(`/users/me/avatar`, {
+            method: "PATCH",
             headers: this._headers,
             body: JSON.stringify({
-                avatar: avatarLink
-              })
-        }) 
+                avatar: avatarLink,
+            }),
+        });
     }
 }
 
-export const api = new Api (config);
-
-
-
-
+export const api = new Api(config);
--- src/components/modal.js
+++ src/components/modal.js
@@ -1,52 +1,115 @@
-const popupProfile = document.querySelector('#profile-popup');
-const popupAddCard = document.querySelector('#add-card-popup');
-const popupAvatar = document.querySelector('#change-avatar');
-const fullSizeImage = document.querySelector('.popup__full-pict');
-const pictureLabel = document.querySelector('.popup__pict-label');
-const cardPicturePopup = document.querySelector('#pict-popup');
+// const popupProfile = document.querySelector("#profile-popup");
+// const popupAddCard = document.querySelector("#add-card-popup");
+// const popupAvatar = document.querySelector("#change-avatar");
+// const cardPicturePopup = document.querySelector("#pict-popup");
+const fullSizeImage = document.querySelector(".popup__full-pict");
+const pictureLabel = document.querySelector(".popup__pict-label");
 
-export const popupController = {
-    openPopup,
-    closePopup,
-    popupProfile,
-    popupAddCard,
-    popupAvatar
-}
+const handleClickClose = function (event) {
+    const isItPopup = event.target.classList.contains("popup");
+    const isItCloseButton = event.target.classList.contains(
+        "popup__container-close-btn"
+    );
 
-const handleClickClose = function (event) {    
-    const isItPopup = event.target.classList.contains('popup');
-    const isItCloseButton = event.target.classList.contains('popup__container-close-btn');
-    
-    if(!isItPopup && !isItCloseButton) {        
+    if (!isItPopup && !isItCloseButton) {
         return;
-    }    
-    
-    const popup = document.querySelector('.popup_opened');   
-    closePopup(popup);
-}
+    }
 
-const handleKeyClose = function (event) {    
-    if(event.key === 'Escape') {
-        const popup = document.querySelector('.popup_opened');
+    const popup = document.querySelector(".popup_opened");
+    closePopup(popup);
+};
+
+const handleKeyClose = function (event) {
+    if (event.key === "Escape") {
+        const popup = document.querySelector(".popup_opened");
         closePopup(popup);
     }
-}
+};
 
 function openPopup(popup) {
-    popup.classList.add('popup_opened');    
-    popup.addEventListener('mousedown', handleClickClose);
-    window.addEventListener('keydown', handleKeyClose);
+    popup.classList.add("popup_opened");
+    popup.addEventListener("mousedown", handleClickClose);
+    window.addEventListener("keydown", handleKeyClose);
 }
 
-function closePopup(popup) {    
-    popup.removeEventListener('mousedown', handleClickClose);
-    window.removeEventListener('keydown', handleKeyClose);
-    popup.classList.remove('popup_opened'); 
+function closePopup(popup) {
+    popup.removeEventListener("mousedown", handleClickClose);
+    window.removeEventListener("keydown", handleKeyClose);
+    popup.classList.remove("popup_opened");
 }
 
-export const handleCardClick = function (image) {    
+export const handleCardClick = function (image) {
     openPopup(cardPicturePopup);
     fullSizeImage.src = image.src;
     fullSizeImage.alt = image.alt;
-    pictureLabel.textContent = image.alt;    
+    pictureLabel.textContent = image.alt;
+};
+
+class Popup {
+    constructor(popupSelector) {
+        this.popupSelector = popupSelector;
+        this.popupElement = document.querySelector(this.popupSelector);
+    }
+    _handleClickClose(event) {
+        const isItPopup = event.target.classList.contains("popup");
+        const isItCloseButton = event.target.classList.contains(
+            "popup__container-close-btn"
+        );
+
+        if (!isItPopup && !isItCloseButton) {
+            return;
+        }
+        closePopup(this.popupElement);
+    }
+    _handleEscClose(event) {
+        if (event.key === "Escape") {
+            closePopup(this.popupElement);
+        }
+    }
+    setEventListeners() {
+        window.addEventListener("keydown", this._handleEscClose);
+        this.popupElement.addEventListener("mousedown", this._handleClickClose);
+    }
+    _removeEventListeners() {
+        window.removeEventListener("keydown", this._handleEscClose);
+        this.popupElement.removeEventListener(
+            "mousedown",
+            this._handleClickClose
+        );
+    }
+    open() {
+        this.popupElement.classList.add("popup_opened");
+        this.setEventListeners();
+    }
+    close() {
+        this.popupElement.classList.remove("popup_opened");
+        this._removeEventListeners();
+    }
 }
+
+class PopupWithImage extends Popup {
+    constructor(popupSelector) {
+        super(popupSelector);
+    }
+    open(src, alt) {
+        super.open();
+        fullSizeImage.src = src;
+        fullSizeImage.alt = alt;
+        pictureLabel.textContent = alt;
+    }
+}
+
+class PopupWithForm extends Popup {
+    constructor(popupSelector, handleFormSubmit) {
+        super(popupSelector);
+        this.handleFormSubmit = handleFormSubmit;
+    }
+    _getInputValues() {}
+}
+
+export const popupController = {
+    popupProfile: new Popup("#profile-popup"),
+    popupAddCard: new Popup("#add-card-popup"),
+    popupAvatar: new Popup("#change-avatar"),
+    cardPicturePopup: new Popup("#pict-popup"),
+};
